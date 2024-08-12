840d9f5a2b2578eb549eb50ba639fb79f82852bc
Gustavo <animex36990@gmail.com>
2024-08-12 14:10:24 -0500

    Avance 2 12/08/2024

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DetallesProductoComponent } from './plataforma/detalles-producto/detalles-producto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'detalles-producto', component: DetallesProductoComponent },
+];
--- src/app/plataforma/contenido/contenido.component.ts
+++ src/app/plataforma/contenido/contenido.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BarraLateralComponent } from '../barra-lateral/barra-lateral.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contenido',
   standalone: true,
-  imports: [BarraLateralComponent],
+  imports: [BarraLateralComponent, RouterLink],
   templateUrl: './contenido.component.html',
   styleUrl: './contenido.component.css'
 })
