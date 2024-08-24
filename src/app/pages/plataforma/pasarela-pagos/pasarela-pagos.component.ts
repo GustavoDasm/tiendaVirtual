784d6c7bb4ab5e66fdf70390b103bc380cfec534
Gustavo <animex36990@gmail.com>
2024-08-24 14:00:39 -0500

    Avance muy pequeño 24/08/2024

--- src/app/pages/plataforma/pasarela-pagos/pasarela-pagos.component.ts
+++ src/app/pages/plataforma/pasarela-pagos/pasarela-pagos.component.ts
@@ -3,6 +3,7 @@ import { RouterLink } from '@angular/router';
 import { ContenidoComponent } from '../contenido/contenido.component';
 import { CarritoService } from '../../../services/carrito.service';
 import { CompraInvitadoComponent } from '../compra-invitado/compra-invitado.component';
+import { Producto } from '../../../interfaces/producto';
 
 @Component({
   selector: 'app-pasarela-pagos',
@@ -13,4 +14,13 @@ import { CompraInvitadoComponent } from '../compra-invitado/compra-invitado.comp
 })
 export class PasarelaPagosComponent {
   carritoService = inject(CarritoService)
+
+
+  aumentarCantidad(producto: Producto){
+    this.carritoService.aumentarCantidadProducto(producto)
+  }
+
+  disminuirCantidad(producto: Producto){
+    this.carritoService.disminuirCantidadProducto(producto)
+  }
 }
