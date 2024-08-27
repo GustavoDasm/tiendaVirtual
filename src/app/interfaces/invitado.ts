ace2ecfca3f696885781ae6a95284919a66c541d
Gustavo <animex36990@gmail.com>
2024-08-27 14:03:06 -0500

    Formulario para compras de invitado 27/08/2024

---
+++ src/app/interfaces/invitado.ts
@@ -0,0 +1,7 @@
+export interface Invitado {
+    nombres: string,
+    apellidos: string,
+    dni: string,
+    telefono: string,
+    correo: string
+}
--- src/app/pages/plataforma/compra-invitado/compra-invitado.component.ts
+++ src/app/pages/plataforma/compra-invitado/compra-invitado.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { CarritoService } from '../../../services/carrito.service';
+import { FormsModule } from '@angular/forms';
+import { Invitado } from '../../../interfaces/invitado';
+import { ContenidoComponent } from '../contenido/contenido.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-compra-invitado',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ContenidoComponent, RouterLink],
   templateUrl: './compra-invitado.component.html',
   styleUrl: './compra-invitado.component.css'
 })
 export class CompraInvitadoComponent {
+  formulario: boolean = false;
+  carritoService = inject(CarritoService);
+  invitado: Invitado = {} as Invitado;
 
+  verificarForm(): void{
+    console.log(this.invitado)
+    this.formulario = true
+  }
 }
