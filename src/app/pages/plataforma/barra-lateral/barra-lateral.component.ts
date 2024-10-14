99d9956eb6ce70576c025079b50467118fe43d73
Gustavo <animex36990@gmail.com>
2024-10-14 14:00:28 -0500

    aaaaa

--- src/app/pages/plataforma/barra-lateral/barra-lateral.component.ts
+++ src/app/pages/plataforma/barra-lateral/barra-lateral.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ContenidoService } from '../contenido/contenido.service';
 
 @Component({
   selector: 'app-barra-lateral',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './barra-lateral.component.css'
 })
 export class BarraLateralComponent {
-
+  service = inject(ContenidoService)
 }
