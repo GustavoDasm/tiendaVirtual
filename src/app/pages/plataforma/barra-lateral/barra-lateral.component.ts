84d6a98b1cfe5053529240bc3bec11fbd5a27141
Gustavo <animex36990@gmail.com>
2024-10-18 14:03:55 -0500

    agregado

--- src/app/pages/plataforma/barra-lateral/barra-lateral.component.ts
+++ src/app/pages/plataforma/barra-lateral/barra-lateral.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { ContenidoService } from '../contenido/contenido.service';
+import { CategoriaService } from '../../../services/categoria.service';
+import { Categoria } from '../../../interfaces/producto';
 
 @Component({
   selector: 'app-barra-lateral',
@@ -9,5 +11,26 @@ import { ContenidoService } from '../contenido/contenido.service';
   styleUrl: './barra-lateral.component.css'
 })
 export class BarraLateralComponent {
+  listaCategorias: Categoria[] = [];
   service = inject(ContenidoService)
+  categoriaService = inject(CategoriaService)
+
+
+  ngOnInit() {
+    this.obtenerCategorias()
+  }
+
+  obtenerCategorias(){
+    this.categoriaService.listarCategorias().subscribe({
+      next: (data) => {
+        if (Array.isArray(data)) {
+          this.listaCategorias = data;
+          console.log(this.listaCategorias)
+        }
+      },
+      error: (err) => {
+        console.log(err.message);
+      }
+    });
+  }
 }
